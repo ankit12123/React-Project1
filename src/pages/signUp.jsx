f35637e5f713270b499644d95fc367e3cd82d776
ankit12123 <ankit3356sahu@gmail.com>
2026-09-01 21:39:32 +0530

    i submiited my signup page also

---
+++ src/pages/signUp.jsx
@@ -0,0 +1,15 @@
+import SignUp from "../components/SignUp page/SignUp";
+
+
+function SignUpPage(){
+    return(
+    <>
+         
+        <SignUp/>
+        
+        
+    </>
+
+    );
+}
+export default SignUpPage;
